1798bd7b3e3ff6e089b72f345633d7e7756bd7a0
Jacqueline Schmatz <di211514@fhstp.ac.at>
2023-05-27 01:14:56 +0200

    Clean external.py acc. to flake8 linting

--- mdi_thesis/external.py
+++ mdi_thesis/external.py
@@ -1,11 +1,18 @@
+"""
+Modle for imports from other sources than GitHub
+"""
 import requests
 
 
 def get_osi_json():
     """
-    Use licenseId to check for matching entries with retrieved data from GitHub to check if license is OSI approved (isOsiApproved)
+    Use licenseId to check for matching entries
+    with retrieved data from GitHub to check,
+    if license is OSI approved (isOsiApproved)
     """
-    url = "https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json"
+    url = \
+        "https://raw.githubusercontent.com/\
+            spdx/license-list-data/master/json/licenses.json"
     response = requests.get(url, timeout=100)
     results_dict = response.json().get("licenses")
     return results_dict
@@ -15,4 +22,6 @@ print(get_osi_json()[0])
 
 
 def get_nvds():
+    """
+    Placeholder"""
     pass
